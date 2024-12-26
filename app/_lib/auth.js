b456991120440638ddf6bc91f694aabc865a33f7
Trần Sơn <transon15042003@gmail.com>
2024-12-26 11:23:01 +0700

    Add auth but keep all sites static

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -2,20 +2,24 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 const authConfig = {
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-  callbacks: {
-    authorized({ auth, request }) {
-      return auth?.user?.name ? true : false;
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        }),
+    ],
+    callbacks: {
+        async session({ session, token, user }) {
+            // Tùy chỉnh session nếu cần
+            return session;
+        },
+        authorized({ auth, request }) {
+            return auth?.user?.name ? true : false;
+        },
     },
-  },
-  pages: {
-    signIn: "/login",
-  },
+    pages: {
+        signIn: "/login",
+    },
 };
 
 export const { auth, handlers, signIn, signOut } = NextAuth(authConfig);
--- app/layout.js
+++ app/layout.js
@@ -1,35 +1,43 @@
+import { ReservationProvider } from "@/app/_contexts/ReservationContext";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import { SessionProvider } from "next-auth/react";
+import { AuthProvider } from "@/app/_contexts/AuthContext";
 import Header from "./_components/Header";
-import { ReservationProvider } from "@/app/_contexts/ReservationContext";
 
 const josefin = Josefin_Sans({
-  subsets: ["latin"],
-  display: "swap",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 export const metadata = {
-  // title: "The Wild Oasis",
-  title: {
-    template: "%s | The Wild Oasis",
-    default: "Welcome to The Wild Oasis",
-  },
-  description: "The Wild Oasis",
+    // title: "The Wild Oasis",
+    title: {
+        template: "%s | The Wild Oasis",
+        default: "Welcome to The Wild Oasis",
+    },
+    description: "The Wild Oasis",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 flex flex-col min-h-screen antialiased`}
-      >
-        <Header />
-        <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">
-            <ReservationProvider>{children}</ReservationProvider>
-          </main>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <SessionProvider>
+            <AuthProvider>
+                <html lang="en">
+                    <body
+                        className={`${josefin.className} bg-primary-950 text-primary-100 flex flex-col min-h-screen antialiased`}
+                    >
+                        <Header />
+                        <div className="flex-1 px-8 py-12 grid">
+                            <main className="max-w-7xl mx-auto w-full">
+                                <ReservationProvider>
+                                    {children}
+                                </ReservationProvider>
+                            </main>
+                        </div>
+                    </body>
+                </html>
+            </AuthProvider>
+        </SessionProvider>
+    );
 }
